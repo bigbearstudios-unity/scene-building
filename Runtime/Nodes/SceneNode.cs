bf3e730004ec3dbb30245e5a8ec8f2bc78120cfd
Stuart Farnaby <stuart@bigbearstudios.co.uk>
2024-09-07 11:25:05 +0100

    Added the basic concepts for the SceneNode

---
+++ Runtime/Nodes/SceneNode.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace BBUnity.SceneBuilding.Nodes {
+
+    [AddComponentMenu("BBUnity/SceneBuilding/Nodes/SceneNode")]
+    class SceneContainer : MonoBehaviour {
+        [SerializeField]
+        private boolean _moveChildrenToRoot = false;
+
+
+    }   
+}
